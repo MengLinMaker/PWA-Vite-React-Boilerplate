fa51982e7cba1ad330707ff7913e23fec6566939
menglinmaker <lm8100mystery@gmail.com>
2022-08-13 16:53:49 +1000

    Made basic boilerplate

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+const faviconURL = '/favicon.svg'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [faviconURL],
+      manifest: {
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: faviconURL,
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          },
+          {
+            src: faviconURL,
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+    })
+  ]
+})
