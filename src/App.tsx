e1e678f212544f0d3b104da5fb2752932ce6d14f
menglinmaker <lm8100mystery@gmail.com>
2022-08-13 23:30:04 +1000

    Make cover page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import reactLogo from './assets/react.svg'
+import sassLogo from './assets/sass.svg'
+import githubLogo from './assets/github.svg'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,19 +15,35 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://sass-lang.com" target="_blank">
+          <img src={sassLogo} className="logo sass" alt="Sass logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <h2>Vite + React + Sass</h2>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more about each too
       </p>
+
+      <div>
+        
+      </div>
+      
+      <div className="card">
+        <a className="center" href="https://github.com/MengLinMaker/PWA-Vite-React-Boilerplate" target="_blank">
+          <button className="credits">
+            <img src={githubLogo} className="logo github" alt="Vite logo" />
+            <p>Meng Lin Maker</p>
+          </button>
+        </a>
+        
+        <h1>
+          <a href="https://github.com/MengLinMaker/PWA-Vite-React-Boilerplate/generate" target="_blank">
+            <button> Use This Boilerplate </button>
+          </a>
+        </h1>
+      </div>
+
     </div>
   )
 }
